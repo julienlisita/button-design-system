03871355a9869116bb0b2526cd43d46fbf50e6dc
Julien Lisita <julien.lisita@gmail.com>
2026-04-12 02:28:37 +0200

    feat: create GroupButton component

--- src/components/Button/GroupButton.tsx
+++ src/components/Button/GroupButton.tsx
@@ -1,19 +1,43 @@
 // src/components/Button/GroupButton.tsx
 
-import TextButtonBase from "./TextButtonBase"
+import Button from "./Button"
 import type { GroupButtonProps } from "./types"
+import './GroupButton.scss'
 
 function GroupButton({
   variant = 'primary',
   addonIcon,
   onAddonClick,
+  className = '',
+  disabled = false,
   ...props
 }: GroupButtonProps) {
+  const groupButtonClassName = [
+    'group-button',
+    `group-button--${variant.toLowerCase()}`,
+    disabled && 'group-button--disabled',
+    className,
+  ]
+    .filter(Boolean)
+    .join(' ')
   return (
-    <div>
-      <TextButtonBase {...props} />
-      <button type="button" onClick={onAddonClick}>
-        {addonIcon}
+    <div className={groupButtonClassName}>
+      <Button 
+        {...props} 
+        variant={variant} 
+        disabled={disabled} 
+        className="group-button__main" 
+      />
+
+      <button 
+        type="button" 
+        className="group-button__addon"
+        onClick={onAddonClick}
+        disabled={disabled}
+      >
+        <span className="group-button__addon-icon" aria-hidden="true">
+          {addonIcon}
+        </span>
       </button>
     </div>
   )
